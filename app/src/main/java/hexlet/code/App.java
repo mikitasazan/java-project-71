40437c1521d95c665fdcbba92432e4547f20a549
Mikita <mikit.sazan@gmail.com>
2026-09-03 11:50:31 +0300

    Добавить позиционные параметры filepath1/filepath2 и опцию -f/--format

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,10 +2,22 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]",
+            defaultValue = "stylish")
+    private String format;
 
     @Override
     public void run() {
